e4a203e3ab1fe89e347ef79e9dff2c17c4380c44
mlstephens <mlstephens@earthlink.net>
2019-01-18 14:51:14 -0800

    Assignment 1 practicing components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { ServerComponent } from './Server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from './warningalert/warningalert.component';
+import { SuccessfulComponent } from './successful/successful.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    WarningAlertComponent,
+    SuccessfulComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/warningalert/warningalert.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+import { sharedStylesheetJitUrl } from '@angular/compiler';
+
+@Component({
+  selector: 'app-warningalert',
+  templateUrl: './warningalert.component.html',
+  styleUrls: ['./warningalert.component.css']
+})
+export class WarningAlertComponent{
+
+}
