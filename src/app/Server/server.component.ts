c06cc18f27740bfd6770c9d41c5fa3086cd29a1c
mlstephens <mlstephens@earthlink.net>
2019-01-20 12:09:21 -0800

    Lecture 35. using nsStyle directive.

--- src/app/Server/server.component.ts
+++ src/app/Server/server.component.ts
@@ -8,8 +8,17 @@ export class ServerComponent
 {
   serverId: number = 10;
   serverStatus: string = 'Offline.';
+  serverName = 'Test-Server';
+
+  constructor() {
+    this.serverStatus = Math.random() > .5 ? 'online' : 'offline';
+  }
 
   getServerStatus() {
     return this.serverStatus;
+  }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';    
   }
 }
